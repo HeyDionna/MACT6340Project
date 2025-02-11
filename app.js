fb7193aca6189cc3fd2c364e2b15f1afc876cad9
HeyDionna <dionnawomack@icloud.com>
2025-02-10 21:42:12 -0600

    Complete EJS

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express"
 import dotenv from "dotenv";
 import * as utils from "./utils/utils.js";
 dotenv.config();
-let data = ("Project 1", "Project 2", "Project 3");
+let data = ["Project 1", "Project 2", "Project 3"];
 
 const app = express();
 const port = 3000;
@@ -16,8 +16,16 @@ app.get("/", (req, res) => {
 
 app.get("/projects", (req, res) => {
   res.render("projects.ejs", {projectArray: data});
-})
-app.get("/project/:id")
+});
+
+app.get("/project/:id", (req, res) => {
+  let id = req.params.id;
+if (id > data.length) {
+  throw new Error("No project with that ID");
+}
+res.render("project.ejs", {projectArray: data, which: id})
+});
+
 app.get("/contact", (req, res) => {
   res.render("contact.ejs");
 })
@@ -31,7 +39,17 @@ app.post("/mail", async (req, res) => {
   .catch(() => {
     res.send({ result: "failure" });
   });
+});
 
+app.use((err, req, res, next) => {
+  console.log(err);
+  let msg;
+  msg = err.message;
+  if (msg != "No project with that ID") {
+    msg =
+      "There was an internal error. Sorry! We're working on cleaning uo the mess.";
+  }
+  res.render("error.ejs", { msg: msg });
 });
 
 app.listen(port, () => {
